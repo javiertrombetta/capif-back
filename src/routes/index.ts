ca19ab0e7049b3c6d9f4307349b058bdfe50daa3
Javier Trombetta <jitrombetta@hotmail.com>
2025-01-23 13:57:19 -0300

    fix: index of routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,12 +11,12 @@ import usersRoutes from './usuariosRoutes';
 
 const router = express.Router();
 
-router.use('/auditorias', authenticate, authorizeRoles(['admin_principal','admin_secundario']), auditoriasRoutes);
+router.use('/audits', authenticate, authorizeRoles(['admin_principal','admin_secundario']), auditoriasRoutes);
 router.use('/auth', authRoutes);
 router.use('/cashflow', authenticate, authorizeRoles(['admin_principal','admin_secundario','productor_principal','productor_secundario']), cashflowRoutes);
-router.use('/conflictos', authenticate, authorizeRoles(['admin_principal','admin_secundario','productor_principal','productor_secundario']), conflictosRoutes);
-router.use('/productoras', authenticate, authorizeRoles(['admin_principal','admin_secundario','productor_principal','productor_secundario']), productorasRoutes);
-router.use('/repertorios', authenticate, authorizeRoles(['admin_principal','admin_secundario','productor_principal','productor_secundario']), repertoriosRoutes);
-router.use('/usuarios', authenticate, usersRoutes);
+router.use('/conflicts', authenticate, authorizeRoles(['admin_principal','admin_secundario','productor_principal','productor_secundario']), conflictosRoutes);
+router.use('/producers', authenticate, authorizeRoles(['admin_principal','admin_secundario','productor_principal','productor_secundario']), productorasRoutes);
+router.use('/repertoires', authenticate, authorizeRoles(['admin_principal','admin_secundario','productor_principal','productor_secundario']), repertoriosRoutes);
+router.use('/users', authenticate, usersRoutes);
 
 export default router;

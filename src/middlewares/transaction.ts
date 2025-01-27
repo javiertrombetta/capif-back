c7b16b3ad77ccb70fb4e6fda6fd03d12cbec4d7d
Javier Trombetta <jitrombetta@hotmail.com>
2025-01-27 09:21:45 -0300

    fix: change transactions for debug

--- src/middlewares/transaction.ts
+++ src/middlewares/transaction.ts
@@ -4,12 +4,32 @@ import sequelize from "../config/database/sequelize";
 export const transactionMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const transaction = await sequelize.transaction();
 
-  try {
-    req.transaction = transaction;
-    await next();
-    await transaction.commit();
-  } catch (error) {
-    await transaction.rollback();
-    next(error);
-  }
+  const rollbackTransaction = async () => {
+    if (transaction) {
+      await transaction.rollback();
+    }
+  };
+
+  const commitTransaction = async () => {
+    if (transaction) {
+      await transaction.commit();
+    }
+  };
+
+  // Escuchar eventos para manejar automáticamente el commit o rollback
+  res.on("finish", async () => {
+    if (res.statusCode >= 200 && res.statusCode < 400) {
+      await commitTransaction();
+    } else {
+      await rollbackTransaction();
+    }
+  });
+
+  res.on("close", async () => {
+    if (!res.headersSent) {
+      await rollbackTransaction();
+    }
+  });
+
+  next();
 };
--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,12 +1,10 @@
 import { JwtPayload } from 'jsonwebtoken';
-import { Transaction } from "sequelize";
 
 declare global {
   namespace Express {
     interface Request {
       user?: string | JwtPayload;
       files?: { [fieldname: string]: Express.Multer.File[] };
-      transaction?: Transaction;
     }
   }
 }

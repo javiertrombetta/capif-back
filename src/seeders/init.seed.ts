05f2232a55b3569a6b987e69c157b0285329167c
Javier Trombetta <jitrombetta@hotmail.com>
2025-02-26 18:42:09 -0300

    fix: change in cargarRepertoriosMasivo

--- src/seeders/init.seed.ts
+++ src/seeders/init.seed.ts
@@ -276,7 +276,6 @@ type VistaData = {
     { nombreVista: "Conflictos", vistaSuperior: "Repertorio" },
     { nombreVista: "Ver Titularidad", vistaSuperior: "Repertorio" },
 
-
     // { nombreVista: "Productoras" },
     { nombreVista: "Premios Gardel", vistaSuperior: "Productoras" },
 
@@ -296,12 +295,8 @@ type VistaData = {
     { nombreVista: "Conflictos", vistaSuperior: "Repertorio" },
     { nombreVista: "Ver Titularidad", vistaSuperior: "Repertorio" },
 
-
     // { nombreVista: "Productoras" },
-    { nombreVista: "Premios Gardel", vistaSuperior: "Productoras" },
-
-    // { nombreVista: "Usuarios" },
-    { nombreVista: "Buscar Usuario", vistaSuperior: "Usuarios" },
+    { nombreVista: "Premios Gardel", vistaSuperior: "Productoras" },    
 
     // { nombreVista: "Cuentas Corrientes" },
     { nombreVista: "Estado de Cuenta", vistaSuperior: "Cuentas Corrientes" },
